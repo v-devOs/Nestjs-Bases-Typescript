4ddc218527bb32bd4e7fa4f0ccd84d7d43b4e1bc
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-26 13:46:12 -0600

    Getters, métodos y THIS

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -14,12 +14,31 @@
 
 export class Pokemon {
 
+  get imageUrl(): string {
+    return `https://pokemon.com/${ this.id }.jpg`;
+  }
+
   constructor( 
     public readonly id: number, 
-    public name: string ){}
+    public name: string,
+    // public imageUrl: string
+  ){}
+
+  scream(){
+    console.log(`${ this.name.toUpperCase() }!!!`)
+
+    this.speak()
+  }
+
+  speak(){
+    console.log(`${ this.name }, ${ this.name }`)
+  }
 }
 
-export const chamander = new Pokemon(4, 'Charmander');
+export const chamander = new Pokemon( 4, 'Charmander' );
 
 // chamander.id = 10;
 // chamander.name = 'Mew'
+console.log(chamander)
+chamander.scream()
+chamander.speak()
