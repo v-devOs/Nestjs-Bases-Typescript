67fff59214aa6b8b3ec9b30723951d970082ee6d
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2024-07-26 15:05:36 -0600

    Genéricos + Sustitución de Liskov

--- src/bases/04-injection.ts
+++ src/bases/04-injection.ts
@@ -1,5 +1,5 @@
 import { Move, PokeapiResponse } from '../interfaces/pokeapi-response.interface';
-import { PokeApiAdapter } from '../api/pokeApi.adapter';
+import { PokeApiAdapter, PokeApiFetchAdapater } from '../api/pokeApi.adapter';
 
 export class Pokemon {
 
@@ -23,7 +23,7 @@ export class Pokemon {
     }
 
     async getMoves(): Promise<Move[]> {
-        const data = await this.http.get('https://pokeapi.co/api/v2/pokemon/4');
+        const data = await this.http.get<PokeapiResponse>('https://pokeapi.co/api/v2/pokemon/4');
         console.log( data.moves );
         
         return data.moves;
@@ -32,7 +32,8 @@ export class Pokemon {
 }
 
 const pokeApi = new PokeApiAdapter()
+const pokeApiFetch = new PokeApiFetchAdapater()
 
-export const charmander = new Pokemon( 4, 'Charmander', pokeApi );
+export const charmander = new Pokemon( 4, 'Charmander', pokeApiFetch );
 
 charmander.getMoves();
